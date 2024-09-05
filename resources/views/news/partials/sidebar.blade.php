b32552952837c769a4bb01a3e19f3d3fb569597c
xirf <andikanp@proton.me>
2024-09-05 15:20:28 +0700

    faet: fatepicker "

--- resources/views/news/partials/sidebar.blade.php
+++ resources/views/news/partials/sidebar.blade.php
@@ -8,25 +8,25 @@
                 <x-heroicon-o-key class="w-5 h-5" />
                 <p>{{ __('Status') }}</p>
             </div>
-            <x-select-input label="ABC">
-                <option value="draft">Draft</option>
-                <option value="publish">Published</option>
-            </x-select-input>
+            <x-select :options="[
+                [
+                    'value' => 'draft',
+                    'label' => 'Draft',
+                ],
+                [
+                    'value' => 'publish',
+                    'label' => 'Published',
+                ],
+            ]" :placeholder="__('Choose')" />
         </div>
-        <div class="flex gap-2 items-center">
-            <div class="flex gap-2 items-center">
-                <x-heroicon-o-eye class="w-5 h-5" />
-                <p>{{ __('Visibility') }}</p>
-            </div>
-        </div>
-        <div class="flex gap-2 items-center">
+        <div class="grid grid-cols-2 gap-4">
             <div class="flex gap-2 items-center">
                 <x-heroicon-o-calendar-days class="w-5 h-5" />
                 <p>{{ __('Publish') }}</p>
             </div>
+            <input type="datetime-local" class="w-full text-sm py-2 px-4 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:border-transparent" />
         </div>
     </div>
     <div class="shadow bg-white sm:rounded-lg space-y-4 p-4 ">
         <h2>{{ __('Tags') }}</h2>
     </div>
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,19 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        'node_modules/preline/dist/*.js',
     ],
-
+    darkMode: false,
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: [ 'Figtree', ...defaultTheme.fontFamily.sans ],
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        require('preline/plugin'),
+    ],
 };
