277e66c420a2f09a905b964b1bc020b14a70fc43
xirf <andikanp@proton.me>
2024-09-10 08:07:07 +0700

    add report

---
+++ database/seeders/ReportModelSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReportModelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
