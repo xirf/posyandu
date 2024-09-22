2b7040c2edf730afa8d99b99793df78c5cba347b
xirf <andikanp@proton.me>
2024-09-22 10:36:33 +0700

    feat: pagination in news

--- resources/views/home/news.blade.php
+++ resources/views/home/news.blade.php
@@ -1,22 +1,22 @@
 <x-home-layout>
     <div class="w-screen space-y-12 py-14">
-        <a href="{{route('news.show', $news[0]->slug)}}">
-        <div class="w-full max-w-7xl mx-auto p-8 grid grid-cols-2 gap-8 rounded-xl bg-white border">
-            <img src="{{ $news[0]->thumbnail }}" alt="{{ $news[0]->title }}"
-                class="h-full aspect-video object-cover rounded-xl border">
-            <div class="flex flex-col gap-2">
-                <div class="flex items-center gap-3">
-                    <img src="{{ $news[0]->user->picture }}" alt="{{ $news[0]->user->name }}"
-                        class="h-8 w-8 object-cover rounded-full">
-                    <div class="font-medium text-sm text-gray-500">{{ $news[0]->user->name }} &middot;
-                        {{ $news[0]->getDiff() }}</div>
+        <a href="{{ route('news.show', $news[0]->slug) }}">
+            <div class="w-full max-w-7xl mx-auto p-8 grid grid-cols-2 gap-8 rounded-xl bg-white border">
+                <img src="{{ $news[0]->thumbnail }}" alt="{{ $news[0]->title }}"
+                    class="h-full aspect-video object-cover rounded-xl border">
+                <div class="flex flex-col gap-2">
+                    <div class="flex items-center gap-3">
+                        <img src="{{ $news[0]->user->picture }}" alt="{{ $news[0]->user->name }}"
+                            class="h-8 w-8 object-cover rounded-full">
+                        <div class="font-medium text-sm text-gray-500">{{ $news[0]->user->name }} &middot;
+                            {{ $news[0]->getDiff() }}</div>
+                    </div>
+                    <h1 class="text-4xl font-bold text-gray-800">{{ $news[0]->title }}</h1>
+                    <p class="text-gray-600 mt-4">{{ $news[0]->getExcerpt(300) }}</p>
+                    <a href="{{ route('news.show', $news[0]->id) }}" class="text-blue-500 mt-4">Read More</a>
                 </div>
-                <h1 class="text-4xl font-bold text-gray-800">{{ $news[0]->title }}</h1>
-                <p class="text-gray-600 mt-4">{{ $news[0]->getExcerpt(300) }}</p>
-                <a href="{{ route('news.show', $news[0]->id) }}" class="text-blue-500 mt-4">Read More</a>
             </div>
-        </div>
-    </a>
+        </a>
         <div class="mx-auto w-full max-w-7xl flex flex-col">
             <h2 class="text-2xl font-bold text-gray-800">Berita Terbaru</h2>
             <div class="grid grid-cols-3 gap-8 mt-8">
@@ -46,6 +46,8 @@
                 @endforeach
             </div>
         </div>
-        
+        <div class="flex space-x-1 mx-auto items-center justify-center">
+            {{ $news->onEachSide(3)->links() }}
+        </div>
     </div>
 </x-home-layout>
